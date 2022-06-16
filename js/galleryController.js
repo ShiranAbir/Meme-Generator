c040fa4a04e39b00d192a1daa70cc9ba85175c60
ShiranAbir <106183696+ShiranAbir@users.noreply.github.com>
2022-06-16 15:34:39 +0300

    Images source fixed.

--- js/galleryController.js
+++ js/galleryController.js
@@ -3,7 +3,7 @@
 function renderGallery() {
     var strHtmls = ''
     gImgs.forEach(img=>{
-        strHtmls += `<img id="${img.id}" onclick="onImgSelect(this)" class="meme-img" src="../img/${img.id}.jpg" alt="">`
+        strHtmls += `<img id="${img.id}" onclick="onImgSelect(this)" class="meme-img" src="img/${img.id}.jpg" alt="">`
     })
     var elGallery = document.querySelector('.gallery')
     elGallery.innerHTML = strHtmls
