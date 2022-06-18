8953915800aaf8b772191af015a9fb771fd2a589
ShiranAbir <106183696+ShiranAbir@users.noreply.github.com>
2022-06-18 14:25:26 +0300

    Added Bonuses Task - Gallery Filter.
    The user can filter the images by keywords : cute ,funny ,cool and cartoon.

--- js/galleryController.js
+++ js/galleryController.js
@@ -2,13 +2,23 @@
 
 function renderGallery() {
     var strHtmls = ''
-    gImgs.forEach(img=>{
-        strHtmls += `<img id="${img.id}" onclick="onImgSelect(this)" class="meme-img" src="img/${img.id}.jpg" alt="">`
-    })
+    if (gFilterdImgs.length > 0) {
+        gFilterdImgs.forEach(img => {
+            strHtmls += `<img id="${img.id}" onclick="onImgSelect(this)" class="meme-img" src="img/${img.id}.jpg" alt="">`
+        })
+    } else {
+        gImgs.forEach(img => {
+            strHtmls += `<img id="${img.id}" onclick="onImgSelect(this)" class="meme-img" src="img/${img.id}.jpg" alt="">`
+        })
+    }
     var elGallery = document.querySelector('.gallery')
     elGallery.innerHTML = strHtmls
 }
 
-function onShowGallery(){
+function onShowGallery() {
     showGallery()
+}
+
+function onSetFilter(filter) {
+    setFilter(filter)
 }
